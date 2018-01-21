04bd9811ef3ff7f7813cd90e581c37fb82fcc9a8
thisistheaj <ajbeckner@gmail.com>
2018-01-20 21:23:33 -0500

    ticketId form

--- src/pages/ticket-id/ticket-id.ts
+++ src/pages/ticket-id/ticket-id.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {TabsPage} from "../tabs/tabs";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 /**
  * Generated class for the TicketIdPage page.
@@ -16,15 +17,27 @@ import {TabsPage} from "../tabs/tabs";
 })
 export class TicketIdPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public form: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: FormBuilder) {
+    this.form = fb.group({
+      ticketId: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+      lastName: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TicketIdPage');
   }
 
-  public login() {
-    this.navCtrl.setRoot(TabsPage);
+  public submitId() {
+    console.log('id', this.form.get('ticketId').value);
+    console.log('lastName', this.form.get('lastName').value);
+    if(this.form.valid){
+      this.navCtrl.push(TabsPage);
+    } else {
+      console.log('invalid field')
+    }
   }
 
 }
